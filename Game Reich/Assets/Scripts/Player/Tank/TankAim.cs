c066567e54ba7e7caad9132dafbcffc576d63a3e
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2022-05-22 11:41:22 +0300

    Shooting

--- Game Reich/Assets/Scripts/Player/Tank/TankAim.cs
+++ Game Reich/Assets/Scripts/Player/Tank/TankAim.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class TankAim : MonoBehaviour
 {
     [SerializeField] private float offset;
+    [SerializeField] private GameObject bulletTankPreFab;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private float bulletForce;
     private void Update()
     {
         Vector3 aim = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotationZ = Mathf.Atan2(aim.y, aim.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotationZ + offset) ;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletTankPreFab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
     }
 }
