85a264279b8c62107af1537d8610b3a1ebb1f844
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2022-05-23 19:52:48 +0300

    CompletedTank

--- Game Reich/Assets/Scripts/Player/Tank/TankAim.cs
+++ Game Reich/Assets/Scripts/Player/Tank/TankAim.cs
@@ -8,14 +8,19 @@ public class TankAim : MonoBehaviour
     [SerializeField] private GameObject bulletTankPreFab;
     [SerializeField] private Transform firePoint;
     [SerializeField] private float bulletForce;
+    private bool shootReady;
+    private void Start()
+    {
+        shootReady = true;
+    }
     private void Update()
     {
         Vector3 aim = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotationZ = Mathf.Atan2(aim.y, aim.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotationZ + offset) ;
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && shootReady)
         {
-            Shoot();
+            StartCoroutine(Wait());
         }
     }
     void Shoot()
@@ -23,5 +28,14 @@ public class TankAim : MonoBehaviour
         GameObject bullet = Instantiate(bulletTankPreFab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        shootReady = false;
+    }
+    IEnumerator Wait()
+    {
+        Shoot();
+
+        yield return new WaitForSeconds(7);
+
+        shootReady = true;
     }
 }
